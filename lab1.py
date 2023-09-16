cc617e696662e188b22372414d06fa404d53a8c1
sk1fix <agafonovtoha427@gmail.com>
2023-09-16 12:07:37 +0400

    csv

--- lab1.py
+++ lab1.py
@@ -1,13 +1,15 @@
 import requests
 import json
+import csv
 
 url = "https://www.cbr-xml-daily.ru/archive/2022/09/08/daily_json.js"
 
 response = requests.get(url)
 data = json.loads(response.text)
 
-
-valute_data = data['Valute']
-for valute in valute_data.values():    
-    print(f"Date: {data['Date']}")
-    print(f"{valute['Name']} rate: {valute['Value']}")
+with open('data.csv', 'w', newline='') as file:
+    valute_data = data['Valute']
+    wr=csv.writer(file)
+    for valute in valute_data.values():    
+        wr.writerow(f"Date: {data['Date']}")
+        wr.writerow(f"{valute['Name']} курс: {valute['Value']}")
