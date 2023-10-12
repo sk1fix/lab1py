1656f99ea22887ee8a0dc959a0f1424b64050bd0
sk1fix <agafonovtoha427@gmail.com>
2023-10-12 09:49:31 +0400

    new

--- lab1.py
+++ lab1.py
@@ -1,11 +1,25 @@
+import datetime
 import json
 import csv
+
 import requests
 
-url = "https://www.cbr-xml-daily.ru/archive/2022/09/08/daily_json.js"
 
-response = requests.get(url)
-data = json.loads(response.text)
+date=datetime.date.today()
+
+print(date)
+while date.year>2014:
+    url = "https://www.cbr-xml-daily.ru/archive/" + date.year + "/" +date.month +"/"+ date.day + "/daily_json.js"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    if date.weekday==0 or date.weekday==6:
+        
+        continue
+    
+
+
+
+
 
 with open('data.csv', 'w', newline='',encoding="utf-8") as file:
     valute_data = data['Valute']
