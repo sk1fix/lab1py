459f87cd3f8027a7a4997d45392ea4b7bc7b5668
sk1fix <agafonovtoha427@gmail.com>
2023-10-21 14:39:36 +0400

    k

--- lab1.py
+++ lab1.py
@@ -6,25 +6,22 @@ import requests
 
 
 date=datetime.date.today()
-
-print(date)
-while date.year>2014:
-    url = "https://www.cbr-xml-daily.ru/archive/" + date.year + "/" +date.month +"/"+ date.day + "/daily_json.js"
-    response = requests.get(url)
-    data = json.loads(response.text)
-    if date.weekday==0 or date.weekday==6:
-        
-        continue
-    
-
-
-
-
-
+date-=datetime.timedelta(days=1)
 with open('data.csv', 'w', newline='',encoding="utf-8") as file:
-    valute_data = data['Valute']
-    wr=csv.writer(file)
-    for valute in valute_data.values():    
-        if valute['CharCode'] == 'USD':
-            wr.writerow((f"Дата: {data['Date']}").split(','))
-            wr.writerow((f"{valute['Name']} курс: {valute['Value']}").split(','))
+    while date.year>2014:
+        url = "https://www.cbr-xml-daily.ru/archive/" + f"{date.year}" + "/" + f"{date.month}" +"/"+ f"{date.day}" + "/daily_json.js"
+        print(url)
+        print(date.weekday())
+        response = requests.get(url)
+        data = json.loads(response.text)
+        if date.weekday()==0 or date.weekday()==6 or date.weekday()==5:
+            date-=datetime.timedelta(days=1)
+            continue
+        valute_data = data['Valute']
+        wr=csv.writer(file)
+        for valute in valute_data.values():    
+            if valute['CharCode'] == 'USD':
+                wr.writerow((f"Дата: {data['Date']}").split(','))
+                wr.writerow((f"{valute['Name']} курс: {valute['Value']}").split(','))
+        date-=datetime.timedelta(days=1)
+    
