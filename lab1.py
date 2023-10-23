3359b1dfab09637bf32d7aa8b28b80ee12fc0cb3
sk1fix <agafonovtoha427@gmail.com>
2023-10-23 13:10:30 +0400

    all dates

--- lab1.py
+++ lab1.py
@@ -9,12 +9,21 @@ date=datetime.date.today()
 date-=datetime.timedelta(days=1)
 with open('data.csv', 'w', newline='',encoding="utf-8") as file:
     while date.year>2014:
-        url = "https://www.cbr-xml-daily.ru/archive/" + f"{date.year}" + "/" + f"{date.month}" +"/"+ f"{date.day}" + "/daily_json.js"
+        o1=""
+        o2=""
+        if date.month<10:
+            o1=0
+        if date.day<10:
+            o2=0
+        url = "https://www.cbr-xml-daily.ru/archive/" + f"{date.year}" + "/" +f"{o1}" + f"{date.month}" +"/"+f"{o2}" f"{date.day}" + "/daily_json.js"
         print(url)
-        print(date.weekday())
+        
         response = requests.get(url)
         data = json.loads(response.text)
-        if date.weekday()==0 or date.weekday()==6 or date.weekday()==5:
+        if 'Valute' not in data:
+            date-=datetime.timedelta(days=1)
+            continue
+        if date.weekday()==0 or date.weekday()==6 :
             date-=datetime.timedelta(days=1)
             continue
         valute_data = data['Valute']
