d690565daabca95301bbc4eac36a99a5507e6227
sk1fix <agafonovtoha427@gmail.com>
2023-11-09 08:35:21 +0400

    fin

--- lab1.py
+++ lab1.py
@@ -5,26 +5,26 @@ import csv
 import requests
 
 
-date=datetime.date.today()
-date-=datetime.timedelta(days=1)
+my_date=datetime.date.today()
+my_date-=datetime.timedelta(days=1)
 with open('data.csv', 'w', newline='',encoding="utf-8") as file:
-    while date.year>2014:
+    while my_date.year>2014:
         o1=""
         o2=""
-        if date.month<10:
+        if my_date.month<10:
             o1=0
-        if date.day<10:
+        if my_date.day<10:
             o2=0
-        url = "https://www.cbr-xml-daily.ru/archive/" + f"{date.year}" + "/" +f"{o1}" + f"{date.month}" +"/"+f"{o2}" f"{date.day}" + "/daily_json.js"
+        url = "https://www.cbr-xml-daily.ru/archive/" + f"{my_date.year}" + "/" +f"{o1}" + f"{my_date.month}" +"/"+f"{o2}" f"{my_date.day}" + "/daily_json.js"
         print(url)
         
         response = requests.get(url)
         data = json.loads(response.text)
         if 'Valute' not in data:
-            date-=datetime.timedelta(days=1)
+            my_date-=datetime.timedelta(days=1)
             continue
-        if date.weekday()==0 or date.weekday()==6 :
-            date-=datetime.timedelta(days=1)
+        if my_date.weekday()==0 or my_date.weekday()==6 :
+            my_date-=datetime.timedelta(days=1)
             continue
         valute_data = data['Valute']
         wr=csv.writer(file)
@@ -32,5 +32,5 @@ with open('data.csv', 'w', newline='',encoding="utf-8") as file:
             if valute['CharCode'] == 'USD':
                 wr.writerow((f"Дата: {data['Date']}").split(','))
                 wr.writerow((f"{valute['Name']} курс: {valute['Value']}").split(','))
-        date-=datetime.timedelta(days=1)
+        my_date-=datetime.timedelta(days=1)
     
